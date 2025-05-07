6ba737c5982914d22f49f672800555c2e260c4b3
yasirmaxstyle <yasirmu77@gmail.com>
2025-05-07 14:28:21 +0700

    fix: fix broken yellow lamp flickering

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ lamp1.classList.add('red')
 //     lamp3.classList.remove('green')
 // })
 
-setInterval(() => {
+function trafficlight () {
     if (lamp1.classList.contains('red')) {
         setTimeout(() => {
             lamp2.classList.add('yellow')
@@ -38,11 +38,35 @@ setInterval(() => {
                         setTimeout(() => {
                             lamp1.classList.add('red')
                             lamp3.classList.remove('green')
+                            trafficlight()
                         }, 3000);
                     }
                 }, 2000);
             }
         }, 3000);
     }
-}, 0);
+}
+
+trafficlight()
+
+// setInterval(() => {
+//     if (lamp1.classList.contains('red')) {
+//         setTimeout(() => {
+//             lamp2.classList.add('yellow')
+//             lamp1.classList.remove('red')
+//             if (lamp2.classList.contains('yellow')) {
+//                 setTimeout(() => {
+//                     lamp3.classList.add('green')
+//                     lamp2.classList.remove('yellow')
+//                     if (lamp3.classList.contains('green')) {
+//                         setTimeout(() => {
+//                             lamp1.classList.add('red')
+//                             lamp3.classList.remove('green')
+//                         }, 3000);
+//                     }
+//                 }, 2000);
+//             }
+//         }, 3000);
+//     }
+// },);
 
